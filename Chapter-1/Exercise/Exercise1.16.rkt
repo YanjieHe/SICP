453cac4b06ea60f4dfb0eff895c8d8e658453466
Yanjie He <heyanjie0@outlook.com>
2019-05-18 13:11:38 -0400

    Update Exercise1.16.rkt

--- Chapter-1/Exercise/Exercise1.16.rkt
+++ Chapter-1/Exercise/Exercise1.16.rkt
@@ -1,1 +1,14 @@
 #lang racket
+
+(define (square x)
+  (* x x))
+
+(define (fast-expt b n)
+  (define (expt-iter b n base result)
+    (cond
+      ((= n 0) result)
+      ((even? n)
+       (expt-iter b (/ n 2) (square base) result))
+      (else
+       (expt-iter b (/ (- n 1) 2) (square base) (* result base)))))
+  (expt-iter b n b 1))
