e57f0fbeec67b7dc1549c7bac6e7a1af3c07be71
Yanjie He <heyanjie0@outlook.com>
2019-05-23 13:06:19 -0400

    Update Tutorial2.1.1.rkt

--- Chapter-2/Tutorial/Tutorial2.1.1.rkt
+++ Chapter-2/Tutorial/Tutorial2.1.1.rkt
@@ -27,3 +27,29 @@
 (define (equal-rat x y)
   (= (* (numer x) (denom y))
      (* (numer y) (denom x))))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+
+(define one-third (make-rat 1 3))
+
+(print-rat (add-rat one-half one-third))
+
+(print-rat (mul-rat one-half one-third))
+
+(print-rat (add-rat one-third one-third))
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
