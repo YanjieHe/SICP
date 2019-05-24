64c59ac55e2a13ce0171f2cbcbe0bf46681bee0a
Yanjie He <heyanjie0@outlook.com>
2019-05-24 17:11:54 -0400

    Update Tutorial2.2.1.rkt

--- Chapter-2/Tutorial/Tutorial2.2.1.rkt
+++ Chapter-2/Tutorial/Tutorial2.2.1.rkt
@@ -10,3 +10,24 @@
 (define squares (list 1 4 9 16 25))
 
 (list-ref squares 3)
+
+;(define (length items)
+;  (if (null? items)
+;      0
+;      (+ 1 (length (cdr items)))))
+
+(define odds (list 1 3 5 7))
+
+(define (length items)
+  (define (length-iter a count)
+    (if (null? a)
+        count
+        (length-iter (cdr a) (+ 1 count))))
+  (length-iter items 0))
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1)
+            (append (cdr list1)
+                    list2))))
