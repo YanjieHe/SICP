0796e4541092eb67895cd964c0988286d5f2d327
Yanjie He <heyanjie0@outlook.com>
2019-05-24 18:00:47 -0400

    Create Exercise2.18.rkt

---
+++ Chapter-2/Exercise/Exercise2.18.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(define (reverse items)
+  (define (iter items result)
+    (if (null? items)
+        result
+        (iter (cdr items) (cons (car items) result))))
+  (iter items '()))
+
+(reverse (list 1 4 9 16 25))
