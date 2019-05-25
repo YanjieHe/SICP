1ff0cc13cc7232d14bc9c6a74674400104249fdf
Yanjie He <heyanjie0@outlook.com>
2019-05-25 18:12:53 -0400

    Update Exercise2.21.rkt

--- Chapter-2/Exercise/Exercise2.21.rkt
+++ Chapter-2/Exercise/Exercise2.21.rkt
@@ -2,7 +2,14 @@
 
 (define nil '())
 
+(define (square x)
+  (* x x))
+
+;(define (square-list items)
+;  (if (null? items)
+;      nil
+;      (cons (square (car items))
+;            (square-list (cdr items)))))
+
 (define (square-list items)
-  (if (null? items)
-      nil
-      (cons 
+  (map square items))
