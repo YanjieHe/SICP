64c22bb77b92d173109da2d0480bdd8425886dec
Yanjie He <heyanjie0@outlook.com>
2019-05-26 12:41:48 -0400

    accumulate -> map

---
+++ Chapter-2/Exercise/Exercise2.33.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define nil '())
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+
+(define (square x) (* x x))
