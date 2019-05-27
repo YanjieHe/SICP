b76bcec16558bfdefc071af35088d09ec4f5a62a
Yanjie He <heyanjie0@outlook.com>
2019-05-26 21:18:04 -0400

    Update Exercise2.34.rkt

--- Chapter-2/Exercise/Exercise2.34.rkt
+++ Chapter-2/Exercise/Exercise2.34.rkt
@@ -7,7 +7,8 @@
           (accumulate op initial (cdr sequence)))))
 
 (define (horner-eval x coefficient-sequence)
-  (accumulate (lambda (this-coeff higher-terms) <??>)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ this-coeff (* x higher-terms)))
               0
               coefficient-sequence))
 
