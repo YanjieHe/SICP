bdb8095a2da664087170e9e3719668d1abeb9265
Yanjie He <heyanjie0@outlook.com>
2019-05-30 18:58:46 -0400

    Update Queue.rkt

--- Chapter-3/Tutorial/Queue.rkt
+++ Chapter-3/Tutorial/Queue.rkt
@@ -1,27 +1,27 @@
 #lang racket
 
-(define (cons x y)
-  (define (set-x! v) (set! x v))
-  (define (set-y! v) (set! y v))
-  (define (dispatch m)
-    (cond ((eq? m 'car) x)
-          ((eq? m 'cdr) y)
-          ((eq? m 'set-car!) set-x!)
-          ((eq? m 'set-cdr!) set-y!)
-          (else (error "Undefinied operation -- CONS" m))))
-  dispatch)
-
-(define (car z) (z 'car))
-
-(define (cdr z) (z 'cdr))
-
-(define (set-car! z new-value)
-  ((z 'set-car!) new-value)
-  z)
-
-(define (set-cdr! z new-value)
-  ((z 'set-cdr!) new-value)
-  z)
+;(define (cons x y)
+;  (define (set-x! v) (set! x v))
+;  (define (set-y! v) (set! y v))
+;  (define (dispatch m)
+;    (cond ((eq? m 'car) x)
+;          ((eq? m 'cdr) y)
+;          ((eq? m 'set-car!) set-x!)
+;          ((eq? m 'set-cdr!) set-y!)
+;          (else (error "Undefinied operation -- CONS" m))))
+;  dispatch)
+;
+;(define (car z) (z 'car))
+;
+;(define (cdr z) (z 'cdr))
+;
+;(define (set-car! z new-value)
+;  ((z 'set-car!) new-value)
+;  z)
+;
+;(define (set-cdr! z new-value)
+;  ((z 'set-cdr!) new-value)
+;  z)
 
 (define (front-ptr queue)
   (car queue))
